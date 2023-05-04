7f7e736250e38564c13b1dc5ea915cf6c38d2a29
alitkovsky <alitkovsky@me.com>
2023-05-04 14:43:22 +0200

    Add recursive binary search function.

--- binary_search.js
+++ binary_search.js
@@ -25,9 +25,31 @@ const binarySearch = (array, item) => {
          start = middle + 1;
       }
    };
-
-   return position;
 };
 
+   // Recursive binary search function
+const recursiveBinarySearch = (array, item, start, end) => {
+   let middle = Math.floor((start + end) / 2); // Define middle element
+   count += 1; // Adding itteration counter
+
+   // Check if defining element is equal to middle we return index of this element. If it's not, we are moving on.
+   if (item === array[middle]) {
+      return middle;
+   }
+   // Check if defining element is smaller than middle we return recursiveBinarySearch() result without middle element and right from middle part of array. If it's not, we return recursiveBinarySearch() result withiout middle element and left from middle part of array.
+   if (item < array[middle]) {
+      return recursiveBinarySearch(array, item, start, middle - 1);
+   } else {
+      return recursiveBinarySearch(array, item, middle + 1, end);
+   } 
+
+};
+
+/* Binary Search
 console.log(binarySearch(array, 4)); // 4
 console.log('count = ', count); // count = 4
+*/
+
+// Recursive Binary Search
+console.log(recursiveBinarySearch(array, 12, 0, array.length)); // 12
+console.log('count = ', count); // count = 12
